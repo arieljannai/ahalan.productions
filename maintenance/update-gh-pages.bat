e98f8ce0ce5700d75a18f2c500d65adec7d63b1d
Ariel Jannai <arieljannai@gmail.com>
2015-08-11 12:33:38 +0300

    preformance

--- maintenance/update-gh-pages.bat
+++ maintenance/update-gh-pages.bat
@@ -1,12 +1,4 @@
 @rem updates the gh-pages branch.
 @rem changing to gh-pages branch -> fetching from master -> merge -> push -> changing back to master
 
-git checkout gh-pages
-
-git fetch
-
-git merge origin/master
-
-git push
-
-git checkout master
+git checkout gh-pages && git fetch&& git merge origin/master && git push && git checkout master
