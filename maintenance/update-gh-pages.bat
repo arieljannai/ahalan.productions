218d26ae033cbf7265691c0a201524c45fd60dd2
Ariel Jannai <arieljannai@gmail.com>
2015-08-11 11:43:44 +0300

    a little script that updates the gh-pages branch based on master

---
+++ maintenance/update-gh-pages.bat
@@ -0,0 +1,8 @@
+@rem updates the gh-pages branch.
+@rem changing to gh-pages branch -> fetching from master -> merge -> push -> changing back to master
+
+git checkout gh-pages;
+git fetch;
+git merge origin/master;
+git push;
+git checkout master;
